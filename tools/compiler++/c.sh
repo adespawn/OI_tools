5b6fa31b6e3dbf3d6daa2234daac419306ffdd07
adespawn <adespawn@gmail.com>
2021-02-15 11:58:56 +0100

    compiler++: added local & no flag  compilation

--- tools/compiler++/c.sh
+++ tools/compiler++/c.sh
@@ -1,7 +1,7 @@
-mn=`whoami`
+mn=$(whoami)
 cache="/home/$mn/.cache/c"
 mydir="$(basename $PWD)"
-if [ -d "$cache" ]; then 
+if [ -d "$cache" ]; then
     e=""
 else
     echo "Nie istnieje ścieżka ustawień: Tworzę $cache"
@@ -14,18 +14,48 @@ if [ -r "$1" ]; then
     #backup kompilowanego pliku
     gnome-terminal -- node ~/.local/bin/bkpCli/index.js ${mydir}_$1 $PWD/$1
     #kompilacja
-    g++ -Wall -Wextra -pedantic -std=c++17 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector $wd/$1 -o $wd/$1.out
+    outdir=$wd/$1.out
+    f1=$1
+    file=$wd/$1
+    flags="-Wall -Wextra -pedantic -std=c++17 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector"
+    while test $# -gt 0; do
+        case "$1" in
+        -nf)
+            # echo $1
+            # echo "XD"
+            flags="-std=c++17"
+            shift
+            ;;
+        -l)
+            # echo "${f1%.*}"
+            outdir="./${f1%.*}"
+            shift
+            ;;
+        *)
+            # echo $1
+            # echo "XX"
+            shift
+            ;;
+        esac
+    done
+
+    # if [ -z "$2" ] || [ "$2" != "-nf" ]; then
+        
+    # else
+    #     flags
+    # fi
+    g++ $flags $file -o $outdir
     errc=$?
-    if [ $errc = "0" ] ; then
-    #kompilacja zakończona sukcesem
-    echo -e 'Skompilowane\n======================================'
+    if [ $errc = "0" ]; then
+        #kompilacja zakończona sukcesem
+        echo -e 'Skompilowane\n======================================'
     else
-    #błąd kompilacji
-    Red='\033[0;31m'
-    echo -e "${Red}Błąd kompilacji"
-    return 1
-    fi 
+        #błąd kompilacji
+        Red='\033[0;31m'
+        echo -e "${Red}Błąd kompilacji"
+        return 1
+    fi
     #wykonanie pliku
-    $wd/$1.out
-    
+    $outdir
+
 fi
