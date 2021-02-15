723c2e7dab8c721f7f4d9afa3b0bf3068eea0002
adespawn <adespawn@gmail.com>
2021-02-15 14:21:25 +0100

    testGenerator

---
+++ tools/testGenerator/index.sh
@@ -0,0 +1,16 @@
+mkdir -p ./bin
+mkdir -p ./paczki/$$/out
+mkdir -p ./paczki/$$/in
+#kompilacja programów
+p1=$(basename -- "$1")
+p1=$$+$p1
+p2=$(basename -- "$2")
+p2=$$+$p2
+g++ $1 -o "./bin/${p1}" -std=c++17
+g++ $2 -o "./bin/${p2}" -std=c++17
+for i in $(bash -c "echo {1..${3}}"); do
+    #generowanie testu
+    ./bin/${p2} $i >./paczki/$$/in/${i}.in
+    #wykonywanie testu na danych programach
+    ./bin/${p1} <./paczki/$$/in/${i}.in >./paczki/$$/out/${i}.out
+done
