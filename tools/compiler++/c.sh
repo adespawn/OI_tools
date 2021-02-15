e68bb596622e94290d8f6bbc7051acbc0c4f0ab9
adespawn <adespawn@gmail.com>
2021-02-15 12:59:24 +0100

    added help

--- tools/compiler++/c.sh
+++ tools/compiler++/c.sh
@@ -1,3 +1,13 @@
+help() {
+    echo "Usage: c file [OPTIONS]"
+    echo ""
+    echo "Compiles and launches given program"
+    echo ""
+    echo "Options"
+    echo "-l\t Places binary loacly"
+    echo "-nf\t Copmpile without flags ( https://codeforces.com/blog/entry/15547 )"
+    echo "-nb\t Do not backup source code ( requries https://github.com/adespawn/OI_tools/tree/main/tools/backup)"
+}
 mn=$(whoami)
 cache="/home/$mn/.cache/c"
 mydir="$(basename $PWD)"
@@ -7,19 +17,28 @@ else
     echo "Nie istnieje ścieżka ustawień: Tworzę $cache"
     mkdir -p "$cache"
 fi
+if [ "$1" = "-h" ] || [ "$1" == "--help" ]; then
+    help
+    return 0
+fi
 if [ -r "$1" ]; then
     wd=$cache/data/$PWD/$1
     mkdir -p $wd
     cp -r $1 $wd/$1
     #backup kompilowanego pliku
-    gnome-terminal -- node ~/.local/bin/bkpCli/index.js ${mydir}_$1 $PWD/$1
+
     outdir=$wd/$1.out
     f1=$1
     file=$wd/$1
+    bp="true"
     flags="-Wall -Wextra -pedantic -std=c++17 -O2 -Wshadow -Wformat=2 -Wfloat-equal -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -fstack-protector"
     #progrm flags handling
     while test $# -gt 0; do
         case "$1" in
+        -h | --help)
+            help
+            return 0
+            ;;
         -nf)
             flags="-std=c++17"
             shift
@@ -28,12 +47,19 @@ if [ -r "$1" ]; then
             outdir="./${f1%.*}"
             shift
             ;;
+        -nb)
+            bp="false"
+            shift
+            ;;
         *)
             shift
             ;;
         esac
     done
     #kompilacja
+    if [ $bp = "true" ]; then
+    gnome-terminal -- node ~/.local/bin/bkpCli/index.js ${mydir}_$1 $PWD/$1
+    fi
     g++ $flags $file -o $outdir
     errc=$?
     if [ $errc = "0" ]; then
